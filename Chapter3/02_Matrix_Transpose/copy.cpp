a082285adcbda4e647ab4d1252eba83cbba7790b
trinayan <trinayan16101993@gmail.com>
2020-08-25 13:19:10 -0400

    Matrix transpose example

---
+++ Chapter3/02_Matrix_Transpose/copy.cpp
@@ -0,0 +1,71 @@
+#include <hip/hip_runtime.h>
+
+#include <cstdlib>
+#include <vector>
+
+const static int width = 32;
+const static int height = 32;
+const static int tile_dim = 32;
+
+__global__ void copy_kernel(float *in, float *out, int width, int height) {
+  int x_index = blockIdx.x * tile_dim + threadIdx.x;
+  int y_index = blockIdx.y * tile_dim + threadIdx.y;
+
+  int index = y_index * width + x_index;
+
+  out[index] = in[index];
+}
+
+
+
+int main() {
+  std::vector<float> matrix_in;
+  std::vector<float> matrix_out;
+
+  matrix_in.resize(width * height);
+  matrix_out.resize(width * height);
+
+  for (int i = 0; i < width * height; i++) {
+    matrix_in[i] = (float)rand() / (float)RAND_MAX;
+  }
+
+  printf("\nInput matrix:\n");
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      printf("%.2f ", matrix_in[i*width + j]);
+    }
+    printf("\n");
+  }
+
+
+
+  float *d_in;
+  float *d_out;
+
+  hipMalloc((void **)&d_in, width * height * sizeof(float));
+  hipMalloc((void **)&d_out, width * height * sizeof(float));
+
+  hipMemcpy(d_in, matrix_in.data(), width * height * sizeof(float),
+            hipMemcpyHostToDevice);
+
+  int block_x = width / tile_dim;
+  int block_y = height / tile_dim;
+
+
+  //GPU Kernel Launch
+
+  hipMemcpy(matrix_out.data(), d_out, width * height * sizeof(float),
+            hipMemcpyDeviceToHost);
+  hipDeviceSynchronize();
+
+  printf("\nOutput matrix:\n");
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      printf("%.2f ", matrix_out[i*width + j]);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
+
